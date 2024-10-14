f781a6778cee53d9abeec9bb5f87efc75acf6031
candelanati <candelanati23@gmail.com>
2024-10-14 16:37:09 -0300

    NavBar filtros

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import './NavBar.css';
+
+function NavBar() {
+    function HandleClick (opcion){
+        console.log("click en: "+opcion)
+    }
+
+    return (
+        <nav>
+            <ul className="navClass">
+                <Filtro HandleClick={HandleClick}>claro</Filtro>
+                <Filtro HandleClick={HandleClick}>oscuro</Filtro>
+            </ul>
+        </nav>
+    );
+}
+
+function Filtro ({children, HandleClick}){
+    return(
+        <li>
+            <button onClick={()=>HandleClick(children)}>{children}</button>
+        </li>
+    )
+
+}
+
+export default NavBar
